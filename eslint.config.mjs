e21a7fad29f96050b226a14eb96e4dde347fb50e
Ilya Tazalov <tazalov9@gmail.com>
2026-03-04 21:10:37 +0300

    feat: pre commit, linters

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,48 @@
-import typescriptEslint from "typescript-eslint";
+import typescriptEslint from 'typescript-eslint'
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended'
 
-export default [{
-    files: ["**/*.ts"],
-}, {
+export default [
+  {
+    files: ['**/*.ts'],
+  },
+  {
     plugins: {
-        "@typescript-eslint": typescriptEslint.plugin,
+      '@typescript-eslint': typescriptEslint.plugin,
     },
 
     languageOptions: {
-        parser: typescriptEslint.parser,
-        ecmaVersion: 2022,
-        sourceType: "module",
+      parser: typescriptEslint.parser,
+      ecmaVersion: 2022,
+      sourceType: 'module',
     },
 
     rules: {
-        "@typescript-eslint/naming-convention": ["warn", {
-            selector: "import",
-            format: ["camelCase", "PascalCase"],
-        }],
+      '@typescript-eslint/naming-convention': [
+        'warn',
+        {
+          selector: 'import',
+          format: ['camelCase', 'PascalCase'],
+        },
+      ],
 
-        curly: "warn",
-        eqeqeq: "warn",
-        "no-throw-literal": "warn",
-        semi: "warn",
+      curly: 'warn',
+      eqeqeq: 'warn',
+      'no-throw-literal': 'warn',
+      semi: 'warn',
     },
-}];
+  },
+  {
+    ...eslintPluginPrettier,
+    rules: {
+      ...eslintPluginPrettier.rules,
+      'prettier/prettier': [
+        'warn',
+        {
+          endOfLine: 'auto',
+          singleQuote: true,
+          semi: false,
+        },
+      ],
+    },
+  },
+]
